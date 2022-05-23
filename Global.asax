73dc979df5f235cc79a0da1084b04945a6f7642f
alisa-shchur <alisa.shur@gmail.com>
2022-05-23 17:59:22 +0400

    Initial commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lab5._2.Global" Language="C#" %>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace lab5._2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.WebService1SoapClient client = new ServiceReference1.WebService1SoapClient();
+            GridView1.DataSource = client.GetClients();
+            GridView1.DataBind();
+        }
+    }
+}
